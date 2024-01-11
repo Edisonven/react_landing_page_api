80f784d66216ee41ab7ed74685b139bf6ed44fda
Edison <aalevenegass@gmail.com>
2024-01-11 18:54:06 -0300

    Ordenando los resultados mapeados desde api con reverse()

--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -18,33 +18,35 @@ const MiApi = ({ setPersonaje, personajes, setError, resultFiltro }) => {
   return (
     <div className="card__container">
       {resultFiltro.length > 0 ? (
-        resultFiltro.map((character) => (
-          <div className="card__body" key={character.id}>
-            <img className="card__img" src={character.image} alt="" />
-            <h2 className="card__name">{character.name}</h2>
-            {character.status === "Alive" ? (
-              <div className="card__status">
-                <p>Estado:</p>
-                <img src="./src/assets/imgs/status_1.svg" alt="" />
-                <p>Alive</p>
-              </div>
-            ) : character.status === "Dead" ? (
-              <div className="card__status">
-                <p>Estado:</p>
-                <img src="./src/assets/imgs/status_2.svg" alt="" />
-                <p>Dead</p>
-              </div>
-            ) : (
-              <div className="card__status">
-                <p>Estado:</p>
-                <img src="./src/assets/imgs/status_3.svg" alt="" />
-                <p>Unknown</p>
-              </div>
-            )}
-            <p>Género: {character.gender}</p>
-            <p>Especie: {character.species}</p>
-          </div>
-        ))
+        resultFiltro
+          .map((character) => (
+            <div className="card__body" key={character.id}>
+              <img className="card__img" src={character.image} alt="" />
+              <h2 className="card__name">{character.name}</h2>
+              {character.status === "Alive" ? (
+                <div className="card__status">
+                  <p>Estado:</p>
+                  <img src="./src/assets/imgs/status_1.svg" alt="" />
+                  <p>Alive</p>
+                </div>
+              ) : character.status === "Dead" ? (
+                <div className="card__status">
+                  <p>Estado:</p>
+                  <img src="./src/assets/imgs/status_2.svg" alt="" />
+                  <p>Dead</p>
+                </div>
+              ) : (
+                <div className="card__status">
+                  <p>Estado:</p>
+                  <img src="./src/assets/imgs/status_3.svg" alt="" />
+                  <p>Unknown</p>
+                </div>
+              )}
+              <p>Género: {character.gender}</p>
+              <p>Especie: {character.species}</p>
+            </div>
+          ))
+          .reverse()
       ) : (
         <h2 className="noCharacter">
           Este personaje no se encuentra en ningún universo, Prueba a buscar
