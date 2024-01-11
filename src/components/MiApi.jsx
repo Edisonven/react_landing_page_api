bbec5123f2c4b49ac26583190cc6822a021e3c08
Edison <aalevenegass@gmail.com>
2024-01-11 13:49:59 -0300

    Creando condicional que evalua si se encuentra el personaje buscado o no

--- src/components/MiApi.jsx
+++ src/components/MiApi.jsx
@@ -17,33 +17,40 @@ const MiApi = ({ setPersonaje, personajes, setError, resultFiltro }) => {
 
   return (
     <div className="card__container">
-      {resultFiltro.map((clave) => (
-        <div className="card__body" key={clave.id}>
-          <img className="card__img" src={clave.image} alt="" />
-          <h2 className="card__name">{clave.name}</h2>
-          {clave.status === "Alive" ? (
-            <div className="card__status">
-              <p>Estado:</p>
-              <img src="./src/assets/imgs/status_1.svg" alt="" />
-              <p>Alive</p>
-            </div>
-          ) : clave.status === "Dead" ? (
-            <div className="card__status">
-              <p>Estado:</p>
-              <img src="./src/assets/imgs/status_2.svg" alt="" />
-              <p>Dead</p>
-            </div>
-          ) : (
-            <div className="card__status">
-              <p>Estado:</p>
-              <img src="./src/assets/imgs/status_3.svg" alt="" />
-              <p>Unknown</p>
-            </div>
-          )}
-          <p>Género: {clave.gender}</p>
-          <p>Especie: {clave.species}</p>
-        </div>
-      ))}
+      {resultFiltro.length > 0 ? (
+        resultFiltro.map((character) => (
+          <div className="card__body" key={character.id}>
+            <img className="card__img" src={character.image} alt="" />
+            <h2 className="card__name">{character.name}</h2>
+            {character.status === "Alive" ? (
+              <div className="card__status">
+                <p>Estado:</p>
+                <img src="./src/assets/imgs/status_1.svg" alt="" />
+                <p>Alive</p>
+              </div>
+            ) : character.status === "Dead" ? (
+              <div className="card__status">
+                <p>Estado:</p>
+                <img src="./src/assets/imgs/status_2.svg" alt="" />
+                <p>Dead</p>
+              </div>
+            ) : (
+              <div className="card__status">
+                <p>Estado:</p>
+                <img src="./src/assets/imgs/status_3.svg" alt="" />
+                <p>Unknown</p>
+              </div>
+            )}
+            <p>Género: {character.gender}</p>
+            <p>Especie: {character.species}</p>
+          </div>
+        ))
+      ) : (
+        <h2 className="noCharacter">
+          Este personaje no se encuentra en ningún universo, Prueba a buscar
+          otra vez!
+        </h2>
+      )}
     </div>
   );
 };
